b843413bcdfe4d54a3464bd3af310311a40cc822
blossom2017 <maitrishastri2016@gmail.com>
2020-07-31 23:16:40 +0530

    Added dummy code to append inputFlatNumber to the table

--- connection_page.php
+++ connection_page.php
@@ -1,6 +1,16 @@
 <?php
 include 'db_connection.php';
+if(isset($_REQUEST))
+{
 $conn = OpenCon();
 echo "Connected Successfully";
+
+$inputFlatNumber = $_POST['inputFlatNumber'];
+$sql = "INSERT INTO temp(inputFlatNumber) VALUES ('$inputFlatNumber')";
+$result = mysqli_query($conn,$sql);
+if($result) {
+	echo "Flat number has been added as into the table successfully.";
+}
+}
 CloseCon($conn);
 ?>
